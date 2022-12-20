2d0aa51e22c37488a820c3365cd924d16fe1f664
Juan Pablo Gaviria <jgavirias13@gmail.com>
2022-12-20 13:21:13 -0500

    [SaveData] Add functionality to persist the changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,11 @@
 import React from 'react';  
 import { AppUi } from './AppUI';
 
-const defaultTodos = [
-  { id: 1, text: 'Cortar Cebolla', completed: false },
-  { id: 2, text: 'Tomar el curso de react', completed: false },
-  { id: 3, text: 'Llorar con la llorona', completed: true },
-  { id: 4, text: 'Tomar el curso de react avanzado', completed: true },
-]
-
 function App() {
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  const savedTodos = !localStorageTodos || localStorageTodos === '' ? [] : JSON.parse(localStorageTodos);
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(savedTodos);
   const [searchValue, setSearchValue] = React.useState('');
   const [filterStatus, setFilterStatus] = React.useState('all');
   const completedTodos = todos.filter(f => !!f.completed).length;
@@ -36,15 +31,22 @@ function App() {
     searchedTodos = filteredStatusTodos.filter(f => f.text.toUpperCase().includes(searchValue.toUpperCase()));
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+  }
+
   const toggleCompleteTodos = (id, status) => {
     todos.find(f => f.id == id).completed = status;
     const newTodosList = [...todos];
     setTodos(newTodosList);
+    saveTodos(newTodosList);
   };
 
   const deleteTodo = (id) => {
     const newTodos = todos.filter(f => f.id != id);
     setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
